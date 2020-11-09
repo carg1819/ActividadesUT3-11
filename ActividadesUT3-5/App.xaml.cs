530d2097d50d025489fcc1fc225cf3307e40c946
Carlos Andres <ca.riano@iesdoctorbalmis.com>
2020-11-09 21:56:12 +0100

    Agregar archivos de proyecto.

---
+++ ActividadesUT3-5/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ActividadesUT3_5
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Evento click_
+        //Con el evento creado hacemos un (MainWindow as MainWindow)Textblock.Text = ...; para recurrir a la clase principal 
+    }
+}
---
+++ ActividadesUT3-5/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActividadesUT3_5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           // Grid contenedor = gridcalc;
+            TextBlock ddawda = new TextBlock
+            {
+                Text = "0",
+            };
+            Viewbox dada = new Viewbox();
+            Button uno = new Button
+            {
+                Tag = "0",
+                
+                             
+            };
+            contenedor.children.Add(uno);
+            Grid.SetRow(uno, 1); Grid.SetColumn(uno, 1);
+            //creo el textblock objeto, new textblock y text
+            //creo el viewbox que tiene .child que será el textblock
+            //metemos el viewbox en el content del boton ej: button1.content = nombreviewbox;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultadoTextBlock.Text = resultadoTextBlock.Text + (sender as Button).Tag.ToString();
+            //Si del sender cogemos el content, casteamos viewbox(cogemos child) > label
+        }
+    }
+}
